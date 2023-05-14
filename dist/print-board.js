ce891b0ad3ede3f458ad7a825adaead42f33c65f
Raquel Adrados <radrados00@gmail.com>
2023-05-14 13:08:15 +0200

    Add print-board file and change directory of styles.scss location

---
+++ dist/print-board.js
@@ -0,0 +1,5 @@
+"use strict";
+const drawBoard = (row, column) => {
+    for (let i = 0, i; ; i++) {
+    }
+};
---
+++ src/print-board.ts
@@ -0,0 +1,6 @@
+const drawBoard = (row: number, column: number) => {
+
+for(let i = 0, i < row; i++){
+
+}
+}
